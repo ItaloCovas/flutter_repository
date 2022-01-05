9f600dd553a04c95c0acd023882783af3e29f5e0
Ítalo Covas <italocovas@gmail.com>
2022-01-05 18:49:58 -0300

    Adding files

---
+++ buscador_gifs/buscador_gifs/lib/ui/home_page.dart
@@ -0,0 +1,161 @@
+import 'dart:convert';
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String? _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if(_search == null || _search!.isEmpty) {
+      response = await http.get(Uri.parse('https://api.giphy.com/v1/gifs/trending?api_key=ZzI7hxGrp5t0wgwI4zIMraf6kv1dj8sZ&limit=20=0&rating=g'));
+    } else {
+      response = await http.get(Uri.parse("https://api.giphy.com/v1/gifs/search?api_key=ZzI7hxGrp5t0wgwI4zIMraf6kv1dj8sZ&q=$_search&limit=19&offset=$_offset&rating=g&lang=en"));
+    }
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map){
+      print(map);
+    });
+  }
+
+  int _getCount(List data) {
+    if(_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquise aqui",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                   _search = text;
+                   _offset = 0;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    case ConnectionState.none:
+                     return Container(
+                       width: 200,
+                       height: 200,
+                       alignment: Alignment.center,
+                       child: CircularProgressIndicator(
+                         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                         strokeWidth: 5,
+                       ),
+                     );
+                     default: 
+                       if(snapshot.hasError) {
+                         return Container();
+                       } else {
+                         return _createGifTable(context, snapshot);
+                       }
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+       ),
+      itemCount: _getCount(snapshot.data["data"]),
+      itemBuilder: (context, index) {
+        if(_search == null || index < snapshot.data["data"].length) {
+          return GestureDetector(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300,
+            fit: BoxFit.cover,
+            ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index])));
+          },
+          onLongPress: () {
+            Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+          },
+        );
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70),
+                  Text('Carregar mais...', style: TextStyle(color: Colors.white, fontSize: 22)),
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
